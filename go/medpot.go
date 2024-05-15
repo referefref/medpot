799e59d43bd82dc5294ad961c35dbc0aa71a9f8b
ref <56499429+referefref@users.noreply.github.com>
2024-05-16 01:26:02 +0800

    Update medpot.go
    Defer connection closure and separate timeout handling
    Graceful EOF handling
    Additional logging for debug
    Anonymous goroutine in main for new connections

--- go/medpot.go
+++ go/medpot.go
@@ -1,294 +1,295 @@
 package main
 
 import (
-	"crypto/tls"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net"
-	"net/http"
-	"os"
-	"strconv"
-	"strings"
-	"time"
+        "crypto/tls"
+        "encoding/base64"
+        "encoding/json"
+        "fmt"
+        "io"
+        "io/ioutil"
+        "net"
+        "net/http"
+        "os"
+        "strconv"
+        "strings"
+        "time"
 
-	"github.com/davecgh/go-spew/spew"
-	"github.com/go-ini/ini"
-	"github.com/mozillazg/request"
-	"go.uber.org/zap"
+        "github.com/davecgh/go-spew/spew"
+        "github.com/go-ini/ini"
+        "github.com/mozillazg/request"
+        "go.uber.org/zap"
 
-	arg "github.com/s9rA16Bf4/ArgumentParser/go/arguments"
-	"github.com/s9rA16Bf4/notify_handler/go/notify"
+        arg "github.com/s9rA16Bf4/ArgumentParser/go/arguments"
+        "github.com/s9rA16Bf4/notify_handler/go/notify"
 )
 
 type conf_t struct {
-	target       string
-	username     string
-	password     string
-	nodeID       string
-	ip           string      // Source ip
-	port         string      // Source port
-	encoded      string      // The encoded (base64) value of what we recieved from the one connecting to us
-	conn         net.Conn    // The active connection
-	log_location string      // Where on the disk is the log file located
-	logger       *zap.Logger // Object to our logger
+        target       string
+        username     string
+        password     string
+        nodeID       string
+        ip           string      // Source ip
+        port         string      // Source port
+        encoded      string      // The encoded (base64) value of what we recieved from the one connecting to us
+        conn         net.Conn    // The active connection
+        log_location string      // Where on the disk is the log file located
+        logger       *zap.Logger // Object to our logger
 }
 
 const (
-	CONN_HOST   = "0.0.0.0"
-	CONN_TYPE   = "tcp"
-	VERSION = "1.0" // Current version
-	CONFIG_LOCATION = "/etc/medpot/"
+        CONN_HOST   = "0.0.0.0"
+        CONN_TYPE   = "tcp"
+        VERSION = "1.0" // Current version
+        CONFIG_LOCATION = "/etc/medpot/"
 )
 
 /*
-	read config from EWS poster for DTAGs Early warning system and T-Pot
+        read config from EWS poster for DTAGs Early warning system and T-Pot
 */
 func readConfig() (string, string, string, string) {
 
-	cfg, err := ini.Load(fmt.Sprintf("%s"+"ews.cfg", CONFIG_LOCATION))
-	if err != nil {
-		notify.Error(err.Error(), "medpot.readConfig()")
-	}
+        cfg, err := ini.Load(fmt.Sprintf("%s"+"ews.cfg", CONFIG_LOCATION))
+        if err != nil {
+                notify.Error(err.Error(), "medpot.readConfig()")
+        }
 
-	target := cfg.Section("EWS").Key("rhost_first").String()
-	user := cfg.Section("EWS").Key("username").String()
-	password := cfg.Section("EWS").Key("token").String()
-	nodeid := cfg.Section("GLASTOPFV3").Key("nodeid").String()
-	nodeid = strings.Replace(nodeid, "glastopfv3-", "medpot-", -1)
-	return target, user, password, nodeid
+        target := cfg.Section("EWS").Key("rhost_first").String()
+        user := cfg.Section("EWS").Key("username").String()
+        password := cfg.Section("EWS").Key("token").String()
+        nodeid := cfg.Section("GLASTOPFV3").Key("nodeid").String()
+        nodeid = strings.Replace(nodeid, "glastopfv3-", "medpot-", -1)
+        return target, user, password, nodeid
 
 }
 
 func post(cconf_t *conf_t, time string) {
 
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
+        tr := &http.Transport{
+                TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+        }
 
-	c := &http.Client{Transport: tr}
-	req := request.NewRequest(c)
+        c := &http.Client{Transport: tr}
+        req := request.NewRequest(c)
 
-	dat := readFile(CONFIG_LOCATION + "ews.xml")
-	body := strings.Replace(string(dat), "_USERNAME_", cconf_t.username, -1)
-	body = strings.Replace(body, "_TOKEN_", cconf_t.password, -1)
-	body = strings.Replace(body, "_NODEID_", cconf_t.nodeID, -1)
-	body = strings.Replace(body, "_IP_", cconf_t.ip, -1)
-	body = strings.Replace(body, "_PORT_", cconf_t.port, -1)
-	body = strings.Replace(body, "_TIME_", time, -1)
-	body = strings.Replace(body, "_DATA_", cconf_t.encoded, -1)
+        dat := readFile(CONFIG_LOCATION + "ews.xml")
+        body := strings.Replace(string(dat), "_USERNAME_", cconf_t.username, -1)
+        body = strings.Replace(body, "_TOKEN_", cconf_t.password, -1)
+        body = strings.Replace(body, "_NODEID_", cconf_t.nodeID, -1)
+        body = strings.Replace(body, "_IP_", cconf_t.ip, -1)
+        body = strings.Replace(body, "_PORT_", cconf_t.port, -1)
+        body = strings.Replace(body, "_TIME_", time, -1)
+        body = strings.Replace(body, "_DATA_", cconf_t.encoded, -1)
 
-	// not set Content-Type
-	req.Body = strings.NewReader(string(body))
-	resp, err := req.Post(cconf_t.target)
+        // not set Content-Type
+        req.Body = strings.NewReader(string(body))
+        resp, err := req.Post(cconf_t.target)
 
-	if err != nil {
-		notify.Warning(fmt.Sprintf("Failed to post, error message attached %s", err.Error()))
-	} else {
-		notify.Inform(fmt.Sprintf("Http reponse status: %s", resp.Status))
-	}
+        if err != nil {
+                notify.Warning(fmt.Sprintf("Failed to post, error message attached %s", err.Error()))
+        } else {
+                notify.Inform(fmt.Sprintf("Http reponse status: %s", resp.Status))
+        }
 
 }
 
 func initLogger(cconf_t *conf_t) *zap.Logger {
 
-	rawJSON := []byte(fmt.Sprintf(`{
-	  "level": "debug",
-	  "encoding": "json",
-	  "outputPaths": ["stdout", "%s"],
-	  "errorOutputPaths": ["stderr"],
-	  "encoderConfig": {
-	    "messageKey": "message",
-	    "levelKey": "level",
-	    "levelEncoder": "lowercase"
-	  }
-	}`, cconf_t.log_location))
+        rawJSON := []byte(fmt.Sprintf(`{
+          "level": "debug",
+          "encoding": "json",
+          "outputPaths": ["stdout", "%s"],
+          "errorOutputPaths": ["stderr"],
+          "encoderConfig": {
+            "messageKey": "message",
+            "levelKey": "level",
+            "levelEncoder": "lowercase"
+          }
+        }`, cconf_t.log_location))
 
-	var cfg zap.Config
-	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
-		notify.Error(err.Error(), "medpot.initLogger()")
-	}
+        var cfg zap.Config
+        if err := json.Unmarshal(rawJSON, &cfg); err != nil {
+                notify.Error(err.Error(), "medpot.initLogger()")
+        }
 
-	logger, err := cfg.Build()
-	if err != nil {
-		notify.Error(err.Error(), "medpot.initLogger()")
-	}
+        logger, err := cfg.Build()
+        if err != nil {
+                notify.Error(err.Error(), "medpot.initLogger()")
+        }
 
-	defer logger.Sync()
+        defer logger.Sync()
 
-	return logger
+        return logger
 }
 
 func main() {
-	arg.Argument_add("--help", "-h", false, "Displays all defined arguments", []string{"NULL"})
-	arg.Argument_add("--set_logo", "-sl", true, "Allows you to pick a logo that is shown on boot, options are = 1, 2", []string{"1", "2"})
-	arg.Argument_add("--set_port", "-sp", true, "Allows for a different port to be used, default = 2575", []string{"NULL"})
-	arg.Argument_add("--set_log_location", "-sll", true, "Changes the directory where the logs will be placed, default = '/var/log/medpot/'", []string{"NULL"})
+    arg.Argument_add("--help", "-h", false, "Displays all defined arguments", []string{"NULL"})
+    arg.Argument_add("--set_logo", "-sl", true, "Allows you to pick a logo that is shown on boot, options are = 1, 2", []string{"1", "2"})
+    arg.Argument_add("--set_port", "-sp", true, "Allows for a different port to be used, default = 2575", []string{"NULL"})
+    arg.Argument_add("--set_log_location", "-sll", true, "Changes the directory where the logs will be placed, default = '/var/log/medpot/'", []string{"NULL"})
 
-	arg.Argument_parse() // Checks which arguments that can have been passed onto the program
+    arg.Argument_parse() // Checks which arguments that can have been passed onto the program
 
-	cconf_t := new(conf_t)
-	var current_logo string
+    cconf_t := new(conf_t)
+    var current_logo string
 
-	if arg.Argument_check("-h") {
-		arg.Argument_help()
-		os.Exit(0)
-	}
+    if arg.Argument_check("-h") {
+        arg.Argument_help()
+        os.Exit(0)
+    }
 
-	if arg.Argument_check("-sl") {
-		switch arg.Argument_get("-sl") {
-		case "1":
-			current_logo = LOGO_1
-		case "2":
-			current_logo = LOGO_2
-		}
-	}else{
-		current_logo = LOGO_2
-	}
+    if arg.Argument_check("-sl") {
+        switch arg.Argument_get("-sl") {
+        case "1":
+            current_logo = LOGO_1
+        case "2":
+            current_logo = LOGO_2
+        }
+    } else {
+        current_logo = LOGO_2
+    }
 
-	if arg.Argument_check("-sp") {
-		cconf_t.port = arg.Argument_get("-sp")
-		_, err := strconv.Atoi(cconf_t.port) // Checks if it's a valid port
-		if err != nil {
-			notify.Error(err.Error(), "medpot.main()")
-		}
-	}else{
-		cconf_t.port = "2575"
-	}
+    if arg.Argument_check("-sp") {
+        cconf_t.port = arg.Argument_get("-sp")
+        _, err := strconv.Atoi(cconf_t.port) // Checks if it's a valid port
+        if err != nil {
+            notify.Error(err.Error(), "medpot.main()")
+        }
+    } else {
+        cconf_t.port = "2575"
+    }
 
-	if arg.Argument_check("-sll") {
-		cconf_t.log_location = arg.Argument_get("-sll")
-	} else {
-		cconf_t.log_location = "/var/log/medpot/"
-	}
-	cconf_t.log_location += "medpot.log"
+    if arg.Argument_check("-sll") {
+        cconf_t.log_location = arg.Argument_get("-sll")
+    } else {
+        cconf_t.log_location = "/var/log/medpot/"
+    }
+    cconf_t.log_location += "medpot.log"
 
+    fmt.Println(current_logo) // Print the logo that will be used
+    notify.Inform(fmt.Sprintf("V.%s", VERSION))
+    notify.Inform(fmt.Sprintf("Starting Medpot at %s", time.Now().Format(time.RFC822)))
+    notify.Inform("Written by @schmalle, forked and updated by @s9rA16Bf4")
+    notify.Inform("If you find any bugs, just report them on the github 'github.com/s9rA16Bf4/medpot'")
+    notify.Inform("--------------------------------------------------------")
+    notify.Inform(fmt.Sprintf("Log files will be located at '%s'", cconf_t.log_location))
+    notify.Inform(fmt.Sprintf("Will utilize port %s", cconf_t.port))
 
-	fmt.Println(current_logo) // Print the logo that will be used
-	notify.Inform(fmt.Sprintf("V.%s", VERSION))
-	notify.Inform(fmt.Sprintf("Starting Medpot at %s", time.Now().Format(time.RFC822)))
-	notify.Inform("Written by @schmalle, forked and updated by @s9rA16Bf4")
-	notify.Inform("If you find any bugs, just report them on the github 'github.com/s9rA16Bf4/medpot'")
-	notify.Inform("--------------------------------------------------------")
-	notify.Inform(fmt.Sprintf("Log files will be located at '%s'", cconf_t.log_location))
-	notify.Inform(fmt.Sprintf("Will utilize port %s", cconf_t.port))
+    cconf_t.target, cconf_t.username, cconf_t.password, cconf_t.nodeID = readConfig()
 
-	cconf_t.target, cconf_t.username, cconf_t.password, cconf_t.nodeID = readConfig()
+    cconf_t.logger = initLogger(cconf_t)
 
-	cconf_t.logger = initLogger(cconf_t)
+    l, err := net.Listen(CONN_TYPE, ":"+cconf_t.port)
+    if err != nil {
+        notify.Error(err.Error(), "medpot.main()")
+    }
+    defer l.Close()
 
-	l, err := net.Listen(CONN_TYPE, ":"+cconf_t.port)
+    notify.Inform(fmt.Sprintf("Listening on host %s on port %s", CONN_HOST, cconf_t.port))
 
-	if err != nil {
-		notify.Error(err.Error(), "medpot.main()")
-	}
-	// Close the listener when the application closes.
-	defer l.Close()
+    for {
+        conn, err := l.Accept()
+        if err != nil {
+            cconf_t.logger.Error("Failed to accept connection", zap.Error(err))
+            notify.Error(fmt.Sprintf("Failed to accept connection: %s", err.Error()), "medpot.main()")
+            continue
+        }
 
-	notify.Inform(fmt.Sprintf("Listening on host %s on port %s", CONN_HOST, cconf_t.port))
-
-	for {
-		// Listen for an incoming connection.
-		cconf_t.conn, err = l.Accept()
-		if err != nil {
-			notify.Error(err.Error(), "medpot.main()")
-		}
-
-		// Handle connections in a new goroutine.
-		go handleRequest(cconf_t)
-	}
+        go func(conn net.Conn) {
+            cconf_t.conn = conn
+            handleRequest(cconf_t)
+        }(conn)
+    }
 }
 
 /*
-	reads file from both possible locations (first repo location, second location from docker install
+        reads file from both possible locations (first repo location, second location from docker install
 */
 func readFile(name string) []byte {
 
-	b1 := make([]byte, 4)
+        b1 := make([]byte, 4)
 
-	dat, err := ioutil.ReadFile(CONFIG_LOCATION + name)
-	if err != nil {
-		notify.Warning(fmt.Sprintf("Failed to read file '%s'", name))
-		return b1
-	}
+        dat, err := ioutil.ReadFile(CONFIG_LOCATION + name)
+        if err != nil {
+                notify.Warning(fmt.Sprintf("Failed to read file '%s'", name))
+                return b1
+        }
 
-	return dat
+        return dat
 }
 
 func handleClientRequest(cconf_t *conf_t, buf []byte, reqLen int) {
-	// These templates are utilized when a user connects
-	dat := readFile("dummyerror.xml")
+        // These templates are utilized when a user connects
+        dat := readFile("dummyerror.xml")
 
-	// copy to a real buffer
-	bufTarget := make([]byte, reqLen)
-	copy(bufTarget, buf)
+        // copy to a real buffer
+        bufTarget := make([]byte, reqLen)
+        copy(bufTarget, buf)
 
-	if strings.Contains(string(buf), "MSH") && strings.Index(string(buf), "MSH|") == 0 {
-		dat = readFile("dummyok.xml")
-	}
+        if strings.Contains(string(buf), "MSH") && strings.Index(string(buf), "MSH|") == 0 {
+                dat = readFile("dummyok.xml")
+        }
 
-	// Send a response back to person contacting us.
-	cconf_t.conn.Write(dat)
+        // Send a response back to person contacting us.
+        cconf_t.conn.Write(dat)
 
 }
 
 // Handles incoming requests.
 func handleRequest(cconf_t *conf_t) {
-	// Make a buffer to hold incoming data.
+    defer func() {
+        notify.Warning("Closing connection")
+        cconf_t.conn.Close()
+        cconf_t.logger.Info("Connection closed")
+    }()
 
-	buf := make([]byte, 1024^2)
-	counter := 0
+    buf := make([]byte, 1024*2) // Correct buffer size
+    counter := 0
 
-	for counter < 3 {
+    for counter < 3 {
+        timeoutDuration := 5 * time.Second // Increase timeout duration
+        cconf_t.conn.SetReadDeadline(time.Now().Add(timeoutDuration))
 
-		timeoutDuration := 3 * time.Second // time out in three seconds from now
-		cconf_t.conn.SetReadDeadline(time.Now().Add(timeoutDuration))
+        reqLen, err := cconf_t.conn.Read(buf)
+        if err != nil {
+            if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+                cconf_t.logger.Warn("Connection timed out", zap.Error(err))
+                notify.Warning(fmt.Sprintf("Connection timed out: %s", err.Error()))
+            } else if err == io.EOF {
+                cconf_t.logger.Info("Client closed the connection", zap.Error(err))
+                notify.Warning(fmt.Sprintf("Client closed the connection: %s", err.Error()))
+                return
+            } else {
+                cconf_t.logger.Error("Error reading from connection", zap.Error(err))
+                notify.Error(fmt.Sprintf("Error reading from connection: %s", err.Error()), "medpot.handleRequest()")
+                return
+            }
+            counter++
+            notify.Inform("Increased counter to " + fmt.Sprint(counter))
+        } else {
+            remote := cconf_t.conn.RemoteAddr().String()
+            cconf_t.ip, cconf_t.port, _ = net.SplitHostPort(remote)
+            currentTime := time.Now().Format(time.RFC822)
 
-		// Read the incoming connection into the buffer.
-		reqLen, err := cconf_t.conn.Read(buf)
-		if err != nil {
-			cconf_t.conn.Close()
+            notify.Inform(fmt.Sprintf("Connection from '%s' on port '%s' at time %s", cconf_t.ip, cconf_t.port, currentTime))
 
-			if err.Error() != "EOF" {
-				notify.Error(err.Error(), "medpot.handleRequest()")
-			}
-			break
+            handleClientRequest(cconf_t, buf, reqLen)
 
-		} else {
+            bufTarget := make([]byte, reqLen)
+            copy(bufTarget, buf)
 
-			remote := cconf_t.conn.RemoteAddr().String()
-			cconf_t.ip, cconf_t.port, _ = net.SplitHostPort(remote)
-			currentTime := time.Now().Format(time.RFC822)
+            spew.Dump(bufTarget)
 
-			notify.Inform(fmt.Sprintf("Connection from '%s' on port '%s' at time %s", cconf_t.ip, cconf_t.port, currentTime))
+            cconf_t.encoded = base64.StdEncoding.EncodeToString(bufTarget)
 
-			handleClientRequest(cconf_t, buf, reqLen)
+            cconf_t.logger.Info("Connection found",
+                zap.String("timestamp", currentTime),
+                zap.String("src_port", cconf_t.port),
+                zap.String("src_ip", cconf_t.ip),
+                zap.String("data", cconf_t.encoded),
+            )
 
-			// copy to a real buffer
-			bufTarget := make([]byte, reqLen)
-			copy(bufTarget, buf)
-
-			spew.Dump(bufTarget)
-
-			cconf_t.encoded = base64.StdEncoding.EncodeToString([]byte(bufTarget))
-
-			cconf_t.logger.Info("Connection found",
-				// Structured context as strongly typed Field values.
-				zap.String("timestamp", currentTime),
-				zap.String("src_port", cconf_t.port),
-				zap.String("src_ip", cconf_t.ip),
-				zap.String("data", cconf_t.encoded),
-			)
-
-			// if configured, send back data to PEBA / DTAG T_pot homebase
-			post(cconf_t, currentTime)
-
-		}
-
-		counter++
-		notify.Inform("Increased counter to " + fmt.Sprint(counter))
-	}
-	notify.Warning("Maximum loop counter reached... loop will now end!")
-	cconf_t.conn.Close()
+            post(cconf_t, currentTime)
+        }
+    }
+    notify.Warning("Maximum loop counter reached... loop will now end!")
 }
